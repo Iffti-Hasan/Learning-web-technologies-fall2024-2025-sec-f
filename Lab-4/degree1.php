cae879ba6b26e1886854cdf8f9a74ecc3980cccd
Khondaker Iffti Hasan Turjo <ifftihasan422@gmail.com>
2024-11-18 16:28:05 +0600

    Add files via upload

---
+++ Lab-4/degree1.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_POST['submit'])) 
+{
+    if (isset($_POST['degree']) && is_array($_POST['degree']) && count($_POST['degree']) >= 2) 
+    {
+        echo "Selected degrees: " . implode(", ", $_POST['degree']);
+    } 
+    else 
+    {
+        echo "Please select at least two degrees.";
+    }
+}
+?>
